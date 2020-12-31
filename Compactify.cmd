608457750a95e7e4decda02521c19bb0c8606271
J. Escott <unknowguy200@hotmail.com>
2021-01-01 01:59:10 +0800

    Update 1/01/2021
    Happy new years from the AWST timezone! Have a small update for Compactify.

--- Compactify.cmd
+++ Compactify.cmd
@@ -1,5 +1,5 @@
 @ECHO OFF
-REM Created by 'Jai the Fox' 2020
+REM Created by 'Jai the Fox' 2021
 
 
 REM Set up some variables here
@@ -11,8 +11,8 @@ REM Cancel out the script if we aren't using Windows 10
 IF NOT EXIST "%SystemRoot%\System32\tar.exe" (
     COLOR 04
     ECHO ERROR!
-    ECHO.
-    ECHO You aren't using a valid Windows system.
+    ECHO(
+    ECHO You aren't using a valid Windows 10 system.
     ECHO Make sure you're using the latest Windows 10 builds.
     TIMEOUT /T -1 2>&1 >NUL
     GOTO EOF
@@ -37,7 +37,7 @@ ECHO ^|      by 'Jai the Fox'      ^|
 ECHO ^|                            ^|
 ECHO ^| Worry no more about space! ^|
 ECHO +----------------------------+
-ECHO.
+ECHO(
 EXIT /B
 
 REM Function to call COMPACT
@@ -62,13 +62,15 @@ SET COMPACT_SUBDIRECTORIES=
 SET BAT_USER_CHOICE=y
 SET /P BAT_USER_CHOICE="Yes / No (Y/N) [Y]? "
 IF /I "%BAT_USER_CHOICE%"=="y" SET COMPACT_SUBDIRECTORIES=1
-ECHO.
+IF DEFINED COMPACT_SUBDIRECTORIES (ECHO [!!] COMPACT will recursively compress all subdirectories.) ELSE (ECHO [!!] COMPACT will NOT recursively compress all subdirectories.)
+ECHO(
 ECHO Do you want COMPACT to force compression ^& recompress compressed files?
 ECHO If you want to skip over already compressed files, just press Enter to input the default 'N'
 SET COMPACT_FORCE=
 SET BAT_USER_CHOICE=n
 SET /P BAT_USER_CHOICE="Yes / No (Y/N) [N]? "
 IF /I "%BAT_USER_CHOICE%"=="y" SET COMPACT_FORCE=1
+IF DEFINED COMPACT_FORCE (ECHO [!!] COMPACT will force compression ^& recompress compressed files.) ELSE (ECHO [!!] COMPACT will NOT force compression ^& recompress compressed files.)
 IF DEFINED COMPACT_SUBDIRECTORIES (
     IF DEFINED COMPACT_FORCE (
         SET COMPACT_PARAMETERS="/C /S /A /I /F /EXE:!COMPACT_COMPRESSION! !COMPACT_FILENAME!"
@@ -83,7 +85,7 @@ IF DEFINED COMPACT_FORCE (
     SET COMPACT_PARAMETERS="/C /A /I /F /EXE:!COMPACT_COMPRESSION! !COMPACT_FILENAME!"
     SET COMPACT_FORCE=
 )
-ECHO.
+ECHO(
 ECHO What compression do you want COMPACT to use?
 ECHO   [XPRESS4K] Fastest, less compression
 ECHO   [XPRESS8K] Balanced, fast ^& adequate compression
@@ -91,22 +93,23 @@ ECHO   [XPRESS16K] Slow, more compression with no additional overhead
 ECHO   [LZX] Slowest, more compression with additional overhead
 SET COMPACT_COMPRESSION="XPRESS8K"
 SET BAT_USER_CHOICE=xpress8k
-SET /P BAT_USER_CHOICE="Compression (XPRESS4K/XPRESS8K/XPRESS16K/LZX) [XPRESS8K]: "
+SET /P BAT_USER_CHOICE="Compression type (XPRESS4K/XPRESS8K/XPRESS16K/LZX) [XPRESS8K]: "
 IF /I "%BAT_USER_CHOICE%"=="xpress4k" SET COMPACT_COMPRESSION="XPRESS4K"
 IF /I "%BAT_USER_CHOICE%"=="xpress16k" SET COMPACT_COMPRESSION="XPRESS16K"
 IF /I "%BAT_USER_CHOICE%"=="lzx" SET COMPACT_COMPRESSION="LZX"
-ECHO.
+ECHO [!!] Using compression type: %COMPACT_COMPRESSION%
+ECHO(
 SET BAT_USER_CHOICE=
 SET /P BAT_USER_CHOICE="OPTIONALLY enter a file/folder name or pattern (multiple allowed): "
-ECHO.
+ECHO(
 ECHO Compressing using COMPACT...
 ECHO ============================
-ECHO.
-ECHO.
+ECHO(
+ECHO(
 CALL :FUNC_COMPACT %COMPACT_PARAMETERS% "%BAT_USER_CHOICE%" %COMPACT_COMPRESSION%
 SET COMPACT_PARAMETERS=
 SET COMPACT_COMPRESSION=
-ECHO.
+ECHO(
 ECHO Note! Any files that get saved/written to will lose their compressed state.
 ECHO For example: if compressed game files are updated, you'll need to recompress them with COMPACT again.
 PAUSE
@@ -118,8 +121,8 @@ CLS
 CALL :FUNC_DISPLAY_HEADER
 ECHO You're about to compress the current directory, are you sure?
 ECHO Current working directory: '%BAT_CWD%'
-ECHO.
-ECHO.
+ECHO(
+ECHO(
 SET BAT_USER_CHOICE=n
 SET /P BAT_USER_CHOICE="Compress (Y/N) [N]? "
 IF /I "%BAT_USER_CHOICE%"=="y" GOTO MENU_COMPRESSOPTS
@@ -136,18 +139,19 @@ SET COMPACT_SUBDIRECTORIES=
 SET BAT_USER_CHOICE=y
 SET /P BAT_USER_CHOICE="Yes / No (Y/N) [Y]? "
 IF /I "%BAT_USER_CHOICE%"=="y" SET COMPACT_SUBDIRECTORIES=1
+IF DEFINED COMPACT_SUBDIRECTORIES (ECHO [!!] COMPACT will recursively uncompress all subdirectories.) ELSE (ECHO [!!] COMPACT will NOT recursively uncompress all subdirectories.)
 IF DEFINED COMPACT_SUBDIRECTORIES (
     SET COMPACT_PARAMETERS="/U /S /A /I /EXE !COMPACT_FILENAME!"
     SET COMPACT_SUBDIRECTORIES=
 )
-ECHO.
+ECHO(
 SET BAT_USER_CHOICE=
 SET /P BAT_USER_CHOICE="OPTIONALLY enter a file/folder name or pattern (multiple allowed): "
-ECHO.
+ECHO(
 ECHO Uncompressing using COMPACT...
 ECHO ==============================
-ECHO.
-ECHO.
+ECHO(
+ECHO(
 CALL :FUNC_COMPACT %COMPACT_PARAMETERS% "%BAT_USER_CHOICE%"
 SET COMPACT_PARAMETERS=
 PAUSE
@@ -159,8 +163,8 @@ CLS
 CALL :FUNC_DISPLAY_HEADER
 ECHO You're about to uncompress the current directory, are you sure?
 ECHO Current working directory: '%BAT_CWD%'
-ECHO.
-ECHO.
+ECHO(
+ECHO(
 SET BAT_USER_CHOICE=n
 SET /P BAT_USER_CHOICE="Uncompress (Y/N) [N]? "
 IF /I "%BAT_USER_CHOICE%"=="y" GOTO MENU_UNCOMPRESSOPTS
@@ -173,8 +177,8 @@ CLS
 CALL :FUNC_DISPLAY_HEADER
 ECHO Querying COMPACT information...
 ECHO ===============================
-ECHO.
-ECHO.
+ECHO(
+ECHO(
 CALL :FUNC_COMPACT "/S /A /Q"
 PAUSE
 GOTO MENU_MAIN
@@ -186,8 +190,8 @@ CALL :FUNC_DISPLAY_HEADER
 ECHO Current working directory: '%BAT_CWD%'
 ECHO If you wish to change this directory, type the directory below.
 ECHO Alternatively, leave it blank to go back without changing it.
-ECHO.
-ECHO.
+ECHO(
+ECHO(
 SET BAT_USER_CHOICE=
 SET /P BAT_USER_CHOICE="New directory: "
 IF NOT DEFINED BAT_USER_CHOICE GOTO MENU_MAIN
@@ -219,8 +223,8 @@ ECHO   [2] Uncompress
 ECHO   [3] Query
 ECHO   [4] Change directory
 ECHO   [5] Quit
-ECHO.
-ECHO.
+ECHO(
+ECHO(
 SET BAT_USER_CHOICE=0
 SET /P BAT_USER_CHOICE="Select: "
 IF %BAT_USER_CHOICE% EQU 1 GOTO MENU_COMPRESS
